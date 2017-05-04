57a02904aee1b9b534e7f7047e8adbe67a1f508e
Albert Tedja <albert@siliconaxon.com>
2017-05-04 06:18:23 -0700

    Initial commit

---
+++ readygroup.go
@@ -0,0 +1,37 @@
+package readygroup
+
+// ReadyGroup is used in opposite situation to sync.WaitGroup.
+// sync.WaitGroup is typically used to wait for goroutines to finish their executions,
+// while ReadyGroup is used to make all goroutines wait before they can begin execution.
+type ReadyGroup struct {
+	groups chan struct{}
+	done   chan struct{}
+	total  uint
+}
+
+// Adds delta, which cannot be negative, to the ReadyGroup counter.
+func (self *ReadyGroup) Add(delta uint) {
+	self.total = delta
+	self.groups = make(chan struct{}, delta)
+	self.done = make(chan struct{}, delta)
+}
+
+// Ready decrements the ReadyGroup counter, and blocks until Go gets unblocked.
+func (self *ReadyGroup) Ready() {
+	self.groups <- struct{}{}
+	<-self.done
+}
+
+// Go blocks until the counter reaches 0.
+func (self *ReadyGroup) Go() {
+	var counter uint = 0
+	for counter < self.total {
+		<-self.groups
+		counter++
+	}
+
+	for counter > 0 {
+		self.done <- struct{}{}
+		counter--
+	}
+}
---
+++ readygroup_test.go
@@ -0,0 +1,37 @@
+package readygroup
+
+import (
+	"testing"
+)
+
+func TestReadyGroup(t *testing.T) {
+	var count uint = 100
+	res := make(chan int, count+1)
+	var rg ReadyGroup
+	rg.Add(count)
+	for i := uint(0); i < count; i++ {
+		go func(n int) {
+			rg.Ready()
+			res <- n
+		}(int(i))
+	}
+	res <- -1
+	rg.Go()
+
+	first := <-res
+	if first != -1 {
+		t.Fatal("Ready does not block!")
+	}
+}
+
+func ExampleReadyGroup() {
+	var rg ReadyGroup
+	rg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func() {
+			rg.Ready() // blocks here until Go gets unblocked.
+			// do something...
+		}()
+	}
+	rg.Go() // blocks and only unblocks when all goroutines are ready.
+}
